292867284fd219034b245196d0404a7b2bf144c3
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2014-04-28 17:56:11 +0000

    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@11 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- OneMainWeb/CommonModules/ArticleList.ascx.cs
+++ OneMainWeb/CommonModules/ArticleList.ascx.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using One.Net.BLL;
 using One.Net.BLL.Web;
 
 namespace OneMainWeb.CommonModules
 {
     public partial class ArticleList : MModule
     {
+        public const string REQUEST_ARTICLE_TEXT_SEARCH = "atsearch";
+        public const string REQUEST_DATE = "adfd";
+
+        private DateTime? requestedMonth;
+        private DateTime? requestedYear;
+        private string requestedArticleTextSearch = "";
+
+        private static readonly BArticle articleB = new BArticle();
 
         #region Settings
         protected string RegularsList { get { return GetStringSetting("RegularsList"); } }
@@ -30,7 +39,36 @@ namespace OneMainWeb.CommonModules
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request[REQUEST_DATE] != null)
+            {
+                string reqDate = Request[REQUEST_DATE];
+                string[] dateVals = reqDate.Trim().Split('-');
+                int reqYear = FormatTool.GetInteger(dateVals[0].Trim());
+                int reqMonth = FormatTool.GetInteger(dateVals[1].Trim());
+                if (reqYear > -1 && reqMonth > -1)
+                    requestedMonth = new DateTime(reqYear, reqMonth, 1);
+                else if (reqYear > -1)
+                    requestedYear = new DateTime(reqYear, 1, 1);
+            }
 
+            // Added June 15 2011 as part of building ArticleDropDownFilter by MP
+            if (Request[REQUEST_ARTICLE_TEXT_SEARCH] != null)
+            {
+                requestedArticleTextSearch = HttpUtility.UrlDecode(Request[REQUEST_ARTICLE_TEXT_SEARCH]);
+            }
+
+            
+            ListingState listingState =
+                    new ListingState(RecordsPerPage, PagerArticles.FirstRecordIndex, SortDescending ? SortDir.Descending : SortDir.Ascending, 
+                    SortByColumn);
+                listingState.OffSet = OffSet; 
+
+           var regulars = RegularsList;
+
+           var articles = articleB.ListArticles(regulars, false, listingState, requestedArticleTextSearch, requestedMonth, requestedYear);
+
+           RepeaterArticles.DataSource = articles;
+           RepeaterArticles.DataBind();
         }
     }
 }
