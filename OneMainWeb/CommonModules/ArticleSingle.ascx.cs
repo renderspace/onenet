469d00a884186e75beaa476ef80e0f998f5a3231
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2014-05-09 14:37:39 +0000

    ArticleSingle
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@18 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- OneMainWeb/CommonModules/ArticleSingle.ascx.cs
+++ OneMainWeb/CommonModules/ArticleSingle.ascx.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using One.Net.BLL.Web;
 
 namespace OneMainWeb.CommonModules
 {
-    public partial class ArticleSingle : System.Web.UI.UserControl
+    public partial class ArticleSingle : MModule
     {
+        protected string ArticleListUri { get { return GetStringSetting("ArticleListUri"); } }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            MultiView1.ActiveViewIndex = 1;
+            Time1.InnerHtml = "bla bla";
+            H1Title.InnerHtml = "title";
+            H2SubTitle.InnerHtml = "subtitle";
+            Time2.Visible = false;
+            SectionTeaser.InnerHtml = "teaser teaser teaser teaser teaser teaser teaser teaser ";
+            SectionHtml.InnerHtml = "html html html html html html html html html html html ";
+            DivReadon.Visible = !string.IsNullOrWhiteSpace(ArticleListUri);
         }
     }
 }
