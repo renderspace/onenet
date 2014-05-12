46613d7aef661af83da2e0656b5cebd6e3053481
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2014-05-12 13:12:30 +0000

    new ArticleSingle
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@19 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- OneMainWeb/CommonModules/ArticleSingle.ascx.cs
+++ OneMainWeb/CommonModules/ArticleSingle.ascx.cs
@@ -4,24 +4,62 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using One.Net.BLL;
 using One.Net.BLL.Web;
 
 namespace OneMainWeb.CommonModules
 {
     public partial class ArticleSingle : MModule
     {
+        public const string REQUEST_ARTICLE_ID = "aid";
+        private static readonly BArticle articleB = new BArticle();
+
         protected string ArticleListUri { get { return GetStringSetting("ArticleListUri"); } }
+        protected bool ShowDate { get { return GetBooleanSetting("ShowDate"); } }
+        protected bool ShowDateBelowTitle { get { return GetBooleanSetting("ShowDateBelowTitle"); } }
+        protected bool ShowSubTitle { get { return GetBooleanSetting("ShowSubTitle"); } }
+        protected bool ShowTeaser { get { return GetBooleanSetting("ShowTeaser"); } }
+        protected string DateFormatString { get { return GetStringSetting("DateFormatString"); } }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            MultiView1.ActiveViewIndex = 1;
-            Time1.InnerHtml = "bla bla";
-            H1Title.InnerHtml = "title";
-            H2SubTitle.InnerHtml = "subtitle";
-            Time2.Visible = false;
-            SectionTeaser.InnerHtml = "teaser teaser teaser teaser teaser teaser teaser teaser ";
-            SectionHtml.InnerHtml = "html html html html html html html html html html html ";
-            DivReadon.Visible = !string.IsNullOrWhiteSpace(ArticleListUri);
+            int articleId = 0;
+            if (Request[REQUEST_ARTICLE_ID] != null)
+            {
+                int.TryParse(Request[REQUEST_ARTICLE_ID], out articleId);
+            }
+
+            var article = articleB.GetArticle(articleId, false);
+
+            if (article != null)
+            {
+
+                MultiView1.ActiveViewIndex = 1;
+
+                Time1.Visible = ShowDate;
+                Time1.Attributes.Add("datetime", article.DisplayDate.ToString("yyyy-MM-dd"));
+                Time1.Attributes.Add("pubdate", article.DateCreated.ToString("yyyy-MM-dd"));
+                Time1.InnerHtml = article.DisplayDate.ToString(DateFormatString);
+                Time2.Visible = ShowDateBelowTitle;
+                Time2.Attributes.Add("datetime", article.DisplayDate.ToString("yyyy-MM-dd"));
+                Time2.Attributes.Add("pubdate", article.DateCreated.ToString("yyyy-MM-dd"));
+                Time2.InnerHtml = article.DisplayDate.ToString(DateFormatString);
+                var id = article.Id.Value;
+                HtmlArticle.Attributes.Add("class", "hentry a" + id.ToString() + " " + MModule.RenderOrder(id));
+                
+                H1Title.InnerHtml = article.Title;
+                H2SubTitle.Visible = ShowSubTitle;
+                H2SubTitle.InnerHtml = article.SubTitle;
+                Time2.Visible = false;
+                SectionTeaser.Visible = ShowTeaser;
+                SectionTeaser.InnerHtml = article.Teaser;
+                SectionHtml.InnerHtml = article.Html;
+                DivReadon.Visible = !string.IsNullOrWhiteSpace(ArticleListUri);
+            }
+            else
+            {
+                MultiView1.ActiveViewIndex = 0;
+            }
         }
     }
 }
