f15b3da3e069f9b3790e476dd679d8215185d9cc
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2014-05-20 08:59:45 +0000

    added
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@32 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

---
+++ OneMainWeb/Controls/GetMeaning.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using One.Net.BLL;
+
+namespace OneMainWeb.Controls
+{
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:GetMeaning runat=server></{0}:GetMeaning>")]
+    public class GetMeaning : WebControl
+    {
+        [Bindable(true)]
+        [Category("Appearance")]
+        [DefaultValue("")]
+        [Localizable(true)]
+        public string Keyword
+        {
+            get
+            {
+                String s = (String)ViewState["Keyword"];
+                return ((s == null) ? String.Empty : s);
+            }
+
+            set
+            {
+                ViewState["Keyword"] = value;
+            }
+        }
+
+        [Bindable(true)]
+        [Category("Appearance")]
+        [DefaultValue("")]
+        [Localizable(true)]
+        public bool Complex
+        {
+            get
+            {
+                if (ViewState["Complex"] == null)
+                    return false;
+                return  (bool)ViewState["Complex"];
+            }
+
+            set
+            {
+                ViewState["Complex"] = value;
+            }
+        }
+        /*
+        protected override void RenderContents(HtmlTextWriter output)
+        {
+            output.Write(Text);
+        }*/
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void Render(HtmlTextWriter output)
+        {
+            if (!Complex)
+                output.Write(BContent.GetMeaning(Keyword));
+            else
+                output.Write(BContent.GetComplexMeaningRendered(Keyword));
+        }
+    }
+}
