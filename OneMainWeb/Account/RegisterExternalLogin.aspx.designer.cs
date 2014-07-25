a2745cb26b11d6dbfa765506a6a6c335caac0d25
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2014-07-25 14:43:32 +0000

    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@94 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- OneMainWeb/Account/RegisterExternalLogin.aspx.designer.cs
+++ OneMainWeb/Account/RegisterExternalLogin.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace OneMainWeb.Account {
     public partial class RegisterExternalLogin {
         
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
         /// userName control.
         /// </summary>
         /// <remarks>
