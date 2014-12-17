b1828888c198c94896f22dc46bb4954691770f2d
mirzap <mirzap@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2014-12-17 10:53:11 +0000

    allowed use of labels instead of placeholders based on setting.
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@303 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- One.Net.Forms/Sql/inserts.sql
+++ One.Net.Forms/Sql/inserts.sql
@@ -10,5 +10,7 @@ VALUES ('Int', 'Form', 'FormId', '-1', 'NORMAL')
 INSERT INTO [dbo].[settings_list]
 ([type], subsystem, [name], default_value, user_visibility)
 VALUES ('Int', 'Form', 'UploadFolderId', '-1', 'NORMAL')
-
+INSERT INTO [dbo].[settings_list]
+([type], subsystem, [name], default_value, user_visibility)
+VALUES ('Bool', 'Form', 'UseLabels', 'false', 'NORMAL')
 -------- END FORM
