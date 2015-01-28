e6ce10e82a089600794d8c082cc40094ce32efb6
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2015-01-28 12:34:04 +0000

    added PageTitle module
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@368 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

---
+++ OneMainWeb/CommonModules/PageTitle.ascx.cs
@@ -0,0 +1,21 @@
+﻿using One.Net.BLL;
+using One.Net.BLL.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OneMainWeb.CommonModules
+{
+    public partial class PageTitle : MModule
+    {
+        private static BWebsite websiteB = new BWebsite();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var page = websiteB.GetPage(PageId);
+            LiteralPageTitle.Text = page.Title;
+        }
+    }
+}
