36cbdde39e1da5191626c2c0a1fc419ee1457778
mirzap <mirzap@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2015-03-09 23:34:48 +0000

    added new module
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@396 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

---
+++ OneMainWeb/CommonModules/TemplateContent.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="TemplateContent.ascx.cs" Inherits="OneMainWeb.CommonModules.TemplateContent" %>
---
+++ OneMainWeb/CommonModules/TemplateContent.ascx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+using One.Net.BLL;
+using One.Net.BLL.Web;
+
+namespace OneMainWeb.CommonModules
+{
+    public partial class TemplateContent : MModule
+    {
+        protected int TemplateId { get { return GetIntegerSetting("TemplateId"); } }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
