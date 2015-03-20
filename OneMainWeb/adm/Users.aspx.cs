476c85b30e566c923ac0093aed070b50bdeb061d
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2015-03-20 14:02:50 +0000

    forms needed trace js function to work. some unknown work in main one.net, too
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@440 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- OneMainWeb/adm/Users.aspx.cs
+++ OneMainWeb/adm/Users.aspx.cs
@@ -129,6 +129,7 @@ namespace OneMainWeb.adm
                 IdentityManager.CreateRoleIfNotExists("Website");
                 IdentityManager.CreateRoleIfNotExists("PublishAllButton");
                 IdentityManager.CreateRoleIfNotExists("AllowDeletePage");
+                IdentityManager.CreateRoleIfNotExists("Users");
 
                 var websiteB = new BWebsite();
                 var list = websiteB.List();
