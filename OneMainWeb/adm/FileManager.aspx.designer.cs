a2b7ec5e153d68a820e610a6144c88fe5392f190
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2015-03-23 09:39:59 +0000

    show path for copying
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@444 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- OneMainWeb/adm/FileManager.aspx.designer.cs
+++ OneMainWeb/adm/FileManager.aspx.designer.cs
@@ -112,6 +112,15 @@ namespace OneMainWeb {
         protected global::System.Web.UI.WebControls.CheckBox CheckBoxConfirm;
         
         /// <summary>
+        /// CheckBoxShowPath control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckBoxShowPath;
+        
+        /// <summary>
         /// LabelFolderId control.
         /// </summary>
         /// <remarks>
