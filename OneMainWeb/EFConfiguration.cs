88f481e6ad18248ac8440cf4f7a009c109b6c6dd
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2015-04-29 08:23:57 +0000

    removed some unused packages and libraries changed the boundles so that they reference minified version by default.html added Azure SQL retry configuration, but this is not proven to work
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@490 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

---
+++ OneMainWeb/EFConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+
+namespace OneMainWeb
+{
+    public class EFConfiguration : DbConfiguration
+    {
+        public EFConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
