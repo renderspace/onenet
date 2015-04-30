31ba46a09927e70a518d34784b52250b18d14d5e
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2015-04-30 08:19:49 +0000

    default styles
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@498 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

---
+++ OneMainWeb/site_specific/ckstyles.js
@@ -0,0 +1,6 @@
+CKEDITOR.stylesSet.add( 'ck_styles', [
+	{ name: 'Heading 2',		element: 'h2' },
+	{ name: 'Heading 3',		element: 'h3' },
+	{ name: 'Heading 4',		element: 'h4' },
+	{ name: 'Clear',		element: 'div', attributes: { 'class': 'clear' } }
+] );
