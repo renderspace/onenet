3c9ad09fb1866ebc4b9b5b6f5de8a5d87e53db37
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2015-05-11 12:13:00 +0000

    version 2.9.6 added save as new to scaffold added disable pager
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@523 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- OneMainWeb/AdminControls/ScaffoldDynamicEditor.ascx.designer.cs
+++ OneMainWeb/AdminControls/ScaffoldDynamicEditor.ascx.designer.cs
@@ -40,6 +40,15 @@ namespace OneMainWeb.AdminControls {
         protected global::System.Web.UI.WebControls.PlaceHolder PanelFieldsHolder;
         
         /// <summary>
+        /// ButtonSaveAsNew control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton ButtonSaveAsNew;
+        
+        /// <summary>
         /// ButtonSave control.
         /// </summary>
         /// <remarks>
