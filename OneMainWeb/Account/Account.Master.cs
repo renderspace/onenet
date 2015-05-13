2f2581ac398a810cb10eb564032960807acae666
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2015-05-13 08:49:43 +0000

    app version added to one.js and one.css to avoid clear cache on upgrade
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@527 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- OneMainWeb/Account/Account.Master.cs
+++ OneMainWeb/Account/Account.Master.cs
@@ -9,6 +9,18 @@ namespace OneMainWeb.Account
 {
     public partial class Account : System.Web.UI.MasterPage
     {
+        protected Version version;
+
+        protected string AppVersion
+        {
+            get
+            {
+                if (version == null)
+                    version = Page.GetType().BaseType.Assembly.GetName().Version;
+                return version.Major + "." + version.Minor + "." + version.Build + "." + version.Revision; //+ version.
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
