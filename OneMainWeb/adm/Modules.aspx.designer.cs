d4364fae0c913a94184829738d2a7f165cc9eb83
samof <samof@6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1>
2015-07-03 13:54:34 +0000

    adding attributes for automatic update of module settings
    git-svn-id: https://rs.unfuddle.com/svn/rs_one-net/trunk@587 6715eb51-551b-4bfe-bfcf-ba04cf7c9ab1

--- OneMainWeb/adm/Modules.aspx.designer.cs
+++ OneMainWeb/adm/Modules.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace OneMainWeb.adm {
         protected global::System.Web.UI.WebControls.View View1;
         
         /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
         /// GridViewModules control.
         /// </summary>
         /// <remarks>
