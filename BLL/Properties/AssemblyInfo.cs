04fd038020adf5b18631987a3fb99a83c4c439e5
Samo Fabcic <samo.fabcic@gmail.com>
2015-07-17 14:37:27 +0200

    new version just for the purposes of custom project with nuget dependency

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.9")]
+[assembly: AssemblyVersion("3.0.2")]
 [assembly: AssemblyKeyName("")]
