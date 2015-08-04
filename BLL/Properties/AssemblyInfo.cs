a58c95198042a75ecf51ba3373e60ff59e3b5e73
Samo Fabcic <samo.fabcic@gmail.com>
2015-08-04 10:31:50 +0200

    3.0.7 which now includes ckfinder connector library

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.0.6")]
+[assembly: AssemblyVersion("3.0.7")]
 [assembly: AssemblyKeyName("")]
