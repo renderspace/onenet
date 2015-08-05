6cb746af84b2e2bde3153c4e0c8965223e01b39a
Samo Fabcic <samo.fabcic@gmail.com>
2015-08-05 14:41:33 +0200

    missing version update

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.0.9.1")]
+[assembly: AssemblyVersion("3.0.10")]
 [assembly: AssemblyKeyName("")]
