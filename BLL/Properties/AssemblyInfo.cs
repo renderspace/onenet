8ecff60253437788ab89c56083f087afb676cc22
Samo Fabcic <samo.fabcic@gmail.com>
2015-08-06 15:05:33 +0200

    added optional custom js after body setting

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.0.10")]
+[assembly: AssemblyVersion("3.0.11")]
 [assembly: AssemblyKeyName("")]
