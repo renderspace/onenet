bb7e7f28ee50ae71807873aee0c37e3a78614708
Samo Fabcic <samo.fabcic@gmail.com>
2015-08-06 14:23:35 +0200

    I think we need p here

--- OneMainWeb/site_specific/ckstyles.js
+++ OneMainWeb/site_specific/ckstyles.js
@@ -1,6 +1,7 @@
 CKEDITOR.stylesSet.add( 'ck_styles', [
 	{ name: 'Heading 2',		element: 'h2' },
 	{ name: 'Heading 3',		element: 'h3' },
-	{ name: 'Heading 4',		element: 'h4' },
+	{ name: 'Heading 4',        element: 'h4' },
+    { name: 'Paragraph ',       element: 'p' },
 	{ name: 'Clear',		element: 'div', attributes: { 'class': 'clear' } }
 ] );
