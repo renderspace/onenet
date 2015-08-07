15c687c1649301902492a0e8f279e53f25641f00
Mirza Pandzo <m_pandzo@yahoo.com.au>
2015-08-07 10:55:27 +0200

    Added new property DisplayFieldCurrent. Added new values to WebSiteDisplayField enum. Added WebSite setting CustomLanguageName .

--- OneMainWeb/Controls/LanguageSelector.cs
+++ OneMainWeb/Controls/LanguageSelector.cs
@@ -22,17 +22,44 @@ namespace OneMainWeb.Controls
             TwoLetterCode,
             ThreeLetterCode,
             NativeName,
-            EnglishName
+            EnglishName,
+            Custom,
+            None
         }
 
         public WebSiteDisplayField DisplayField { get; set; }
+        public WebSiteDisplayField DisplayFieldCurrent { get; set; }
         public int Group { get; set; }
 
         protected readonly static BWebsite webSiteB = new BWebsite();
+        private IEnumerable<BOWebSite> WebSiteList;
 
         public LanguageSelector()
         {
             base.EnableViewState = false;
+        }
+
+        protected override void CreateChildControls()
+        {
+            WebSiteList = webSiteB.List();
+
+            base.CreateChildControls();
+        }
+
+        private BOWebSite CurrentWebSite
+        {
+            get
+            {
+                if (WebSiteList != null)
+                {
+                    foreach (var webSite in WebSiteList)
+                    {
+                        if (webSite.Language.LCID == Thread.CurrentThread.CurrentCulture.LCID)
+                            return webSite;
+                    }
+                }
+                return null;
+            }
         }
 
         public override void RenderBeginTag(System.Web.UI.HtmlTextWriter writer)
@@ -43,10 +70,13 @@ namespace OneMainWeb.Controls
             if (!string.IsNullOrEmpty(CssClass))
                 writer.AddAttribute(HtmlTextWriterAttribute.Class, this.CssClass);
             writer.RenderBeginTag("nav");
+
             writer.AddAttribute(HtmlTextWriterAttribute.Class, "current");
             writer.RenderBeginTag("span");
-            writer.Write(Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName);
+            if (CurrentWebSite != null)
+                RenderDisplayField(CurrentWebSite, writer, DisplayFieldCurrent);
             writer.RenderEndTag();
+
             writer.RenderBeginTag("ul");
         }
 
@@ -63,53 +93,76 @@ namespace OneMainWeb.Controls
 
         protected override void RenderContents(HtmlTextWriter writer)
         {
-            var list = webSiteB.List();
+            var list = WebSiteList;
 
-            foreach (var website in list)
+            if (list != null)
             {
-                if (website.WebSiteGroup == Group && Group > 0)
+                foreach (var website in list)
                 {
-                    var websiteUri = PublishFlag ? website.ProductionUrl : website.PreviewUrl;
-
-                    if (!string.IsNullOrEmpty(websiteUri))
+                    if (website.WebSiteGroup == Group && Group > 0)
                     {
-                        var cssClass = "lang-" + website.Language.TwoLetterISOLanguageName;
-                        if (Thread.CurrentThread.CurrentCulture.LCID == website.Language.LCID)
-                            cssClass += " current";
+                        var websiteUri = PublishFlag ? website.ProductionUrl : website.PreviewUrl;
 
-                        writer.AddAttribute(HtmlTextWriterAttribute.Class, cssClass);
-                        writer.RenderBeginTag("li");
-
-                        writer.AddAttribute(HtmlTextWriterAttribute.Href, websiteUri);
-                        writer.AddAttribute(HtmlTextWriterAttribute.Title, website.Title);
-                        writer.RenderBeginTag("a");
-
-                        switch (DisplayField)
+                        if (!string.IsNullOrEmpty(websiteUri))
                         {
-                            case WebSiteDisplayField.TwoLetterCode :
-                                writer.Write(website.Language.TwoLetterISOLanguageName);
-                                break;
-                            case WebSiteDisplayField.ThreeLetterCode:
-                                writer.Write(website.Language.ThreeLetterISOLanguageName);
-                                break;
-                            case WebSiteDisplayField.NativeName:
-                                writer.Write(website.Language.NativeName);
-                                break;
-                            case WebSiteDisplayField.EnglishName:
-                                writer.Write(website.Language.EnglishName);
-                                break;
-                            default:
-                                writer.Write(website.Language.TwoLetterISOLanguageName);
-                                break;
+                            var cssClass = "lang-" + website.Language.TwoLetterISOLanguageName;
+                            if (Thread.CurrentThread.CurrentCulture.LCID == website.Language.LCID)
+                                cssClass += " current";
+
+                            writer.AddAttribute(HtmlTextWriterAttribute.Class, cssClass);
+                            writer.RenderBeginTag("li");
+
+                            writer.AddAttribute(HtmlTextWriterAttribute.Href, websiteUri);
+                            writer.AddAttribute(HtmlTextWriterAttribute.Title, website.Title);
+                            writer.RenderBeginTag("a");
+
+                            RenderDisplayField(website, writer, DisplayField);
+
+                            writer.RenderEndTag();
+                            writer.RenderEndTag();
                         }
-                        
-                        writer.RenderEndTag();
-                        writer.RenderEndTag();
                     }
                 }
             }
 
             base.RenderContents(writer);
+        }
+
+        private void RenderDisplayField(BOWebSite website, HtmlTextWriter writer, WebSiteDisplayField displayField)
+        {
+            switch (displayField)
+            {
+                case WebSiteDisplayField.TwoLetterCode:
+                    writer.Write(website.Language.TwoLetterISOLanguageName);
+                    break;
+                case WebSiteDisplayField.ThreeLetterCode:
+                    writer.Write(website.Language.ThreeLetterISOLanguageName);
+                    break;
+                case WebSiteDisplayField.NativeName:
+                    writer.Write(website.Language.NativeName);
+                    break;
+                case WebSiteDisplayField.EnglishName:
+                    writer.Write(website.Language.EnglishName);
+                    break;
+                case WebSiteDisplayField.Custom:
+                    writer.Write(website.CustomLanguageName);
+                    break;
+                case WebSiteDisplayField.None:
+                    break;
+                default:
+                    writer.Write(website.Language.TwoLetterISOLanguageName);
+                    break;
+            }
+        }
+
+        private static string SentenceCase(string input)
+        {
+            if (input.Length < 1)
+                return input;
+
+            string sentence = input.ToLower();
+            return sentence[0].ToString().ToUpper() +
+               sentence.Substring(1);
         }
 
     }
