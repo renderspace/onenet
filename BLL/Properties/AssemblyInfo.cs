c4a4c6165d96047a4ccfb2230f675f12c57de71f
Samo Fabcic <samo.fabcic@gmail.com>
2015-08-13 16:58:26 +0200

    new clear cache

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.0.19")]
+[assembly: AssemblyVersion("3.0.21")]
 [assembly: AssemblyKeyName("")]
---
+++ OneMainWeb/Utils/OneClear.aspx.cs
@@ -0,0 +1,42 @@
+﻿using One.Net.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OneMainWeb.Utils
+{
+    public partial class OneClear : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var selectedWebSiteId = PresentBasePage.ReadWebSiteId();
+
+            var websiteB = new BWebsite();
+            var currentWebsite = websiteB.Get(selectedWebSiteId);
+
+            var publishFlag = PresentBasePage.ReadPublishFlag();
+
+            if (!publishFlag && !string.IsNullOrWhiteSpace(currentWebsite.PreviewUrl))
+            {
+                Button1.Text = "Clear cache on preview";
+            }
+            if (publishFlag && !string.IsNullOrWhiteSpace(currentWebsite.ProductionUrl))
+            {
+                Button1.Text = "Clear cache on production";
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            BWebsite webSiteB = new BWebsite();
+            webSiteB.ClearCache();
+            OneSiteMapProvider.ReloadSiteMap();
+            RouteConfig.ReloadRoutes(RouteTable.Routes);
+            Label1.Text = DateTime.Now.ToString();
+        }
+    }
+}
