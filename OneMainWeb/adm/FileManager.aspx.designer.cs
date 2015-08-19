33ebcd6b880226dce5189795b74980f60564b87a
Samo Fabcic <samo.fabcic@gmail.com>
2015-08-19 12:09:38 +0200

    replaced confirm delete checkbox with boostrap confirmation as elsewhere

--- OneMainWeb/adm/FileManager.aspx.designer.cs
+++ OneMainWeb/adm/FileManager.aspx.designer.cs
@@ -103,15 +103,6 @@ namespace OneMainWeb {
         protected global::System.Web.UI.WebControls.HiddenField HiddenFieldLanguageId;
         
         /// <summary>
-        /// CheckBoxConfirm control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox CheckBoxConfirm;
-        
-        /// <summary>
         /// CheckBoxShowPath control.
         /// </summary>
         /// <remarks>
