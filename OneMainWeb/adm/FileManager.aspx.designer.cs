4d7974b643ca2e85e5def47a326c3b123fb92677
Samo Fabcic <samo.fabcic@gmail.com>
2015-08-19 14:41:47 +0200

    removed unused checkbox

--- OneMainWeb/adm/FileManager.aspx.designer.cs
+++ OneMainWeb/adm/FileManager.aspx.designer.cs
@@ -103,15 +103,6 @@ namespace OneMainWeb {
         protected global::System.Web.UI.WebControls.HiddenField HiddenFieldLanguageId;
         
         /// <summary>
-        /// CheckBoxShowPath control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox CheckBoxShowPath;
-        
-        /// <summary>
         /// LabelFolderId control.
         /// </summary>
         /// <remarks>
