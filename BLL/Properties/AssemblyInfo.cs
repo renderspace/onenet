1e71908e4e49bbd4de6164d13089f51f750e8d87
Mirza Pandzo <m_pandzo@yahoo.com.au>
2015-09-01 16:15:31 +0200

    upped version number to 3.0.32 to incorporate Samo's merged changes.

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.0.31")]
+[assembly: AssemblyVersion("3.0.32")]
 [assembly: AssemblyKeyName("")]
