3933a63dd908541959eea162bd685f1c1f68e661
Samo Fabcic <samo.fabcic@gmail.com>
2015-09-22 09:45:55 +0200

    upped version for marko changes

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.0.35")]
+[assembly: AssemblyVersion("3.0.36")]
 [assembly: AssemblyKeyName("")]
