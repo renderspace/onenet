ad20c33b8c2ba44e64fed8307557b7dd8e53a6fd
Mirza Pandzo <m_pandzo@yahoo.com.au>
2015-09-25 13:28:59 +0200

    match other characters and replace duplicates with dash: @"[-_,+.^&'""=*?=#]{2,}"

--- BLL/Core/BWebsite.cs
+++ BLL/Core/BWebsite.cs
@@ -261,6 +261,9 @@ namespace One.Net.BLL
             answer = answer.Replace("Č", "C");
             answer = answer.Replace("Ð", "D");
 
+            var regex = new Regex(@"[-_,+.^&'""=*?=#]{2,}"); // match 2 or more occurences of - _ ? * ' '' & ^ . , = + # characters
+            answer = regex.Replace(answer, "-"); // and replace them with a single dash
+
             answer = answer.Replace("/", "-");
             answer = answer.Replace(" ", "-");
             answer = answer.Replace("?", "");
