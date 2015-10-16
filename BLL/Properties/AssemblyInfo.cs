d6cb9591ec796101d32ac2924082510e67c23879
Samo Fabcic <samo.fabcic@gmail.com>
2015-10-16 09:53:26 +0200

    upped version just to asure the deployment to zrsz

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.0.50")]
+[assembly: AssemblyVersion("3.0.51")]
 [assembly: AssemblyKeyName("")]
