7252e32b33d28cc2c29c370608a79e667a929165
Mirza Pandzo <m_pandzo@yahoo.com.au>
2015-11-04 16:11:46 +0100

    Added ExtraCssClass setting to cms Page. Upped version to 3.1.1

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyVersion("3.1.1")]
 [assembly: AssemblyKeyName("")]
