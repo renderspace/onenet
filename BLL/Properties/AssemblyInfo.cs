53c7cd6d305d97fc9d954cbe802c0fe367fd293e
Samo Fabcic <samo.fabcic@gmail.com>
2015-11-05 15:59:14 +0100

    3.1.3

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.1.2")]
+[assembly: AssemblyVersion("3.1.3")]
 [assembly: AssemblyKeyName("")]
