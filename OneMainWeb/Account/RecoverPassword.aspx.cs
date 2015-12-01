96ad22230c474975cb9222712ef2d0cbebd30bfc
Mirza Pandzo <m_pandzo@yahoo.com.au>
2015-12-01 09:49:48 +0100

    Implemented password recovery.

--- OneMainWeb/Account/RecoverPassword.aspx.cs
+++ OneMainWeb/Account/RecoverPassword.aspx.cs
@@ -14,13 +14,25 @@ namespace OneMainWeb.Account
     public partial class RecoverPassword : System.Web.UI.Page
     {
         protected static Logger log = LogManager.GetCurrentClassLogger();
+        protected string Token { get; set; }
+        protected string UserId { get; set; }
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                if (Request[IdentityHelper.CodeKey] != null)
+                    Token = Request[IdentityHelper.CodeKey];
+                if (Request[IdentityHelper.UserIdKey] != null)
+                    UserId = Request[IdentityHelper.UserIdKey];
 
+                if (!string.IsNullOrEmpty(Token) && !string.IsNullOrEmpty(UserId))
+                    MultiView1.ActiveViewIndex = 1;
+                else
+                    MultiView1.ActiveViewIndex = 0;
+            }
         }
-
-
+        
         protected void Recover_Click(object sender, EventArgs e)
         {
             if (IsValid)
@@ -41,6 +53,32 @@ namespace OneMainWeb.Account
                 {
                     log.Info("Invalid email. " + Email.Text);
                     FailureText.Text = "Invalid email.";
+                    ErrorMessage.Visible = true;
+                }
+            }
+        }
+
+        protected void Reset_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                var manager = new UserManager();
+                var user = manager.FindById(UserId);
+
+                if (user != null)
+                {
+                    log.Info("Reset password request... UserId:" + UserId);
+
+                    var newPassword = NewPassword.Text;
+
+                    manager.ResetPassword(user.Id, Token, newPassword);
+                    manager.SendEmail(user.Id, "Password has been reset", "Your password was successfully reset.");
+                    MultiView1.ActiveViewIndex = 1;
+                }
+                else
+                {
+                    log.Info("Invalid email for reset attempt. UserId" + UserId);
+                    FailureText.Text = "Invalid user.";
                     ErrorMessage.Visible = true;
                 }
             }
