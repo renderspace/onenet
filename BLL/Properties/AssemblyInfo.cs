a90069eb775eb074e648f684ab4009c16f5a2855
Samo Fabcic <samo.fabcic@gmail.com>
2016-01-13 16:49:47 +0100

    fix runat=server occasional problem

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.3.1")]
+[assembly: AssemblyVersion("3.3.2")]
 [assembly: AssemblyKeyName("")]
