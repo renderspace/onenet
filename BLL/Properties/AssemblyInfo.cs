8e1bbf374632c3f305d8264f3920d3029f0fdaac
Mirza Pandzo <m_pandzo@yahoo.com.au>
2016-02-25 11:53:02 +0100

    Delete selected button was not shown when folder had files. Fixed.

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.3.12")]
+[assembly: AssemblyVersion("3.3.13")]
 [assembly: AssemblyKeyName("")]
