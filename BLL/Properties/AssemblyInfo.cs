06ca4961bead9042b1143572b44d0dc6378cf079
Mirza Pandzo <m_pandzo@yahoo.com.au>
2016-03-31 11:05:02 +0200

    Added Group setting to breadcrumb upped version to 3.3.17

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.3.16")]
+[assembly: AssemblyVersion("3.3.17")]
 [assembly: AssemblyKeyName("")]
