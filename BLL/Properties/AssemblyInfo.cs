5b00493c41fd8d4b3799fbedb1828f578ab60974
Mirza Pandzo <m_pandzo@yahoo.com.au>
2016-04-06 11:15:02 +0200

    fixed issue with duplicate keys. upped version 3.3.20

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.3.19")]
+[assembly: AssemblyVersion("3.3.20")]
 [assembly: AssemblyKeyName("")]
