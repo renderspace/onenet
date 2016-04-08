2984aba53e3e7096854db2347d79e56f4f89414e
Mirza Pandzo <m_pandzo@yahoo.com.au>
2016-04-08 16:32:41 +0200

    upped to 3.3.24 upgraded one.forms to latest version

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.3.23")]
+[assembly: AssemblyVersion("3.3.24")]
 [assembly: AssemblyKeyName("")]
