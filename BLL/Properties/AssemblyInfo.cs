55207a14c730ef9f29651c152eaf784aa2a4473c
Samo Fortuna <samo.4tuna@gmail.com>
2016-06-07 15:33:34 +0200

    upgraded forms; and to VS2015

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.3.34")]
+[assembly: AssemblyVersion("3.3.35")]
 [assembly: AssemblyKeyName("")]
