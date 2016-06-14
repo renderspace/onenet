26b19fce12894d7cdf2e91e02add257d8754e96d
Mirza Pandzo <m_pandzo@yahoo.com.au>
2016-06-14 14:46:18 +0200

    if ShowPager is off, do not do paging.

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.3.37")]
+[assembly: AssemblyVersion("3.3.38")]
 [assembly: AssemblyKeyName("")]
