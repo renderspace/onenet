cbbfebd567cf3011b36ac2a32536bb041ca1cc1e
Mirza Pandzo <m_pandzo@yahoo.com.au>
2016-06-23 11:55:45 +0200

    fixed leftover reordering bug

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.3.41")]
+[assembly: AssemblyVersion("3.3.42")]
 [assembly: AssemblyKeyName("")]
