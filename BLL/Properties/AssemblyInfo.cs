6723409d7ef821cae0bc76f1175be72b6d2e70c4
Samo Fortuna <samo.4tuna@gmail.com>
2016-07-07 14:00:50 +0200

    upped version

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.3.47")]
+[assembly: AssemblyVersion("3.3.48")]
 [assembly: AssemblyKeyName("")]
