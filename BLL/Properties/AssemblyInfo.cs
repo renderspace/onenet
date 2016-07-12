4296fe5565bd20684bba25f209ebaae1b1283068
Samo Fortuna <samo.4tuna@gmail.com>
2016-07-12 16:47:25 +0200

    3.3.52

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.3.51")]
+[assembly: AssemblyVersion("3.3.52")]
 [assembly: AssemblyKeyName("")]
