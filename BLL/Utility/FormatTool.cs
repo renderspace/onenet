1f74b0a6c306653252ad2a4a06a72726a4de6b75
Samo Fortuna <samo.4tuna@gmail.com>
2016-07-12 12:54:29 +0200

    improved JSON output - date field type

--- BLL/Utility/FormatTool.cs
+++ BLL/Utility/FormatTool.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -6,6 +7,14 @@ namespace One.Net.BLL
 {
     public static class FormatTool
     {
+        public static double UnixTicks(this DateTime dt)
+        {
+            DateTime d1 = new DateTime(1970, 1, 1);
+            DateTime d2 = dt.ToUniversalTime();
+            TimeSpan ts = new TimeSpan(d2.Ticks - d1.Ticks);
+            return ts.TotalMilliseconds;
+        }
+
         public static string ToCommaSeparatedValues<T>(this List<T> list)
         {
             return ToSeparatedValues(list, ",");
