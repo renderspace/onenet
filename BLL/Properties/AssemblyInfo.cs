932d676be814fc21f90fb2aa8b5238910f5d9b0f
Samo Fortuna <samo.4tuna@gmail.com>
2016-07-12 17:09:09 +0200

    3.3.53

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.3.52")]
+[assembly: AssemblyVersion("3.3.53")]
 [assembly: AssemblyKeyName("")]
--- OneMainWeb/Properties/AssemblyInfo.cs
+++ OneMainWeb/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.52")]
-[assembly: AssemblyFileVersion("3.3.52")]
+[assembly: AssemblyVersion("3.3.53")]
+[assembly: AssemblyFileVersion("3.3.53")]
