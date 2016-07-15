d3bc2944d68017bad057218347d1adcecaf2feb7
Samo Fortuna <samo.4tuna@gmail.com>
2016-07-15 10:32:07 +0200

    ArticleSingle now hides images based on class not setting

--- OneMainWeb/SiteDefault/ckstyles.js
+++ OneMainWeb/SiteDefault/ckstyles.js
@@ -1,7 +1,15 @@
-CKEDITOR.stylesSet.add( 'ck_styles', [
-	{ name: 'Heading 2',		element: 'h2' },
-	{ name: 'Heading 3',		element: 'h3' },
-	{ name: 'Heading 4',        element: 'h4' },
-    { name: 'Paragraph ',       element: 'p' },
-	{ name: 'Clear',		element: 'div', attributes: { 'class': 'clear' } }
-] );
+CKEDITOR.stylesSet.add('ck_styles', [
+	{ name: 'Heading 2', element: 'h2' },
+	{ name: 'Heading 3', element: 'h3' },
+	{ name: 'Heading 4', element: 'h4' },
+    { name: 'Paragraph ', element: 'p' },
+	{
+	    name: 'Clear',
+	    element: 'div', attributes: { 'class': 'clear' }
+	},
+    {
+        name: 'Display image in gallery',
+        element: 'img',
+        attributes: { 'class': 'display-in-gallery' }
+    }
+]);
