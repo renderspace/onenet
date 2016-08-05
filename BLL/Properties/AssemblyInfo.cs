844a7c6d74464e962a75ac1d58859e502736a1e5
Mirza Pandzo <m_pandzo@yahoo.com.au>
2016-08-05 10:25:11 +0200

    upped to 3.6.4

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.6.3")]
+[assembly: AssemblyVersion("3.6.4")]
 [assembly: AssemblyKeyName("")]
