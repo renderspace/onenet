9ebc8fe27218e3a10c493897f2032ac4977a10fa
Mirza Pandzo <m_pandzo@yahoo.com.au>
2017-09-12 11:35:31 +0200

    3.9.36 added default value for title argument of RenderLink for backward compat

--- OneMainWeb/CommonModules/ArticleList.ascx.cs
+++ OneMainWeb/CommonModules/ArticleList.ascx.cs
@@ -212,7 +212,7 @@ namespace OneMainWeb.CommonModules
             }
         }
 
-        protected string RenderLink(string humanReadableUrl, string title)
+        protected string RenderLink(string humanReadableUrl, string title = "")
         {
             var link = SingleArticleUri + "/" + humanReadableUrl;
             return String.Format("<a href='{0}' title='{1}'>{2}</a>", link, title, title);
