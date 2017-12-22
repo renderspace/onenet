1230aeb63ef892baecc3287190a7729935eca08f
samofab <samo.fabcic@gmail.com>
2017-12-22 09:59:14 +0100

    4.0.0

--- OneMainWeb/Properties/AssemblyInfo.cs
+++ OneMainWeb/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Renderspace d.o.o.")]
 [assembly: AssemblyProduct("One.NET MainWeb")]
-[assembly: AssemblyCopyright("Copyright © Renderspace d.o.o. 2017")]
+[assembly: AssemblyCopyright("Copyright © Renderspace d.o.o. 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.9.42")]
-[assembly: AssemblyFileVersion("3.9.42")]
+[assembly: AssemblyVersion("4.0.0")]
+[assembly: AssemblyFileVersion("4.0.0")]
