6617776ec374c8e068e279ef2abe6325ab663778
Samo 4 <samo.4tuna@gmail.com>
2018-08-09 14:21:48 +0200

    4.0.18 - articlelist year filter fix

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.0.17")]
+[assembly: AssemblyVersion("4.0.18")]
 [assembly: AssemblyKeyName("")]
