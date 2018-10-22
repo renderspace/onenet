d70fb6eda3965469a52dc754ee7bb1649a621cad
Mirza Pandzo <m_pandzo@yahoo.com.au>
2018-10-22 17:42:56 +0200

    simple search module

---
+++ OneMainWeb/CommonModules/SimpleSearch.ascx
@@ -0,0 +1,28 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SimpleSearch.ascx.cs" Inherits="OneMainWeb.CommonModules.SimpleSearch" %>
+
+<div class="searchForm">	
+	<div class="input searchInput">
+		<input name="_searchText" id="_searchText" type="text" class="searchText" />
+		<a id="_searchButton" class="searchButton" onclick="return _doSearch();"><%=Translate("do_search") %></a>
+	</div>
+</div>
+
+<script type="text/javascript">
+    function _doSearch()
+	{
+        var str = document.getElementById('_searchText').value;
+		if (str != '')
+			window.location.href = '<%=SearchResultsUri%>' + "?q=" + str;
+		return false;
+    }
+
+	st = document.getElementById("_searchText");
+	if (st) {
+		st.addEventListener("keyup", function (event) {
+			event.preventDefault();
+			if (event.keyCode == 13) {
+				document.getElementById("_searchButton").click();
+			}
+		});
+	}
+</script>
---
+++ OneMainWeb/CommonModules/SimpleSearch.ascx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Threading;
+using One.Net.BLL;
+using One.Net.BLL.Web;
+using One.Net.BLL.Model.Attributes;
+using One.Net.BLL.WebControls;
+
+namespace OneMainWeb.CommonModules
+{
+    public partial class SimpleSearch : MModule
+    {
+        [Setting(SettingType.String, DefaultValue = "")]
+        public string SearchResultsUri { get { return GetStringSetting("SearchResultsUri"); } }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request["q"] != null)
+            {
+                var searchTerm = Request["q"].ToString();
+                var uri = "/AdminService/SearchPageContent?keyword=" + searchTerm + "&languageId=" + Thread.CurrentThread.CurrentCulture.LCID;
+
+                var client = new WebClient();
+                string response = client.DownloadString(uri);
+
+                Response.Write(response);
+                Response.End();
+            }
+        }
+    }
+}
