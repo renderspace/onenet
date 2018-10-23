582808e4b4f3860868d8862b9165230eeab4a3a7
Mirza Pandzo <m_pandzo@yahoo.com.au>
2018-10-23 16:11:02 +0200

    4.1.5

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.1.4")]
+[assembly: AssemblyVersion("4.1.5")]
 [assembly: AssemblyKeyName("")]
