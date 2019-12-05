8ecb293e24a5b6d6ff2e0279aacc1594285613de
Samo 4 <samo.4tuna@gmail.com>
2019-12-05 19:06:59 +0100

    copyright 2020

--- OneMainWeb/Properties/AssemblyInfo.cs
+++ OneMainWeb/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Renderspace d.o.o.")]
 [assembly: AssemblyProduct("One.NET MainWeb")]
-[assembly: AssemblyCopyright("Copyright © Renderspace d.o.o. 2018")]
+[assembly: AssemblyCopyright("Copyright © Renderspace d.o.o. 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
